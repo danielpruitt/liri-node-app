bc35da4fc802dace1a53ce780f973af0215efdea
danielpruitt <35474097+danielpruitt@users.noreply.github.com>
2018-07-20 20:51:03 -0400

    spotify and twitter functions

--- assets/javascript/keys.js
+++ assets/javascript/keys.js
@@ -1,10 +1,10 @@
 console.log("this is loaded");
 
 module.exports.twitter={
-    consumer_key: process.env.pnzgbjgpgpWEEH3zZEtmgybAD,
-    consumer_secret: process.env.ebi6KONjZtzmyMd25dRVZlVfbMnyb29xd1uUPGR684WOXektNP,
-    access_token_key: process.env.1018625254325997570-4WnGOHoGMj6m8skYsnKNcIv9beG6Xu,
-    access_token_secret: process.env.wLIa0kcN2PocWepKywePDR8Gv7pK6e6kqHs8HkQ1Jvk4j,
+    consumer_key: process.env.TWITTER_CONSUMER_KEY,
+    consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
+    access_token_key: process.env.TWITTER_ACCESS_TOKEN,
+    access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET,
 };
 
 module.exports.spotify = {
--- assets/javascript/liri.js
+++ assets/javascript/liri.js
@@ -1,9 +1,87 @@
 require('dotenv').config();
 
-var needed = require("./liri.js");
-
+var keys = require("./keys.js");
+var Twitter = require("twitter");
+var Spotify = require("node-spotify-api");
+// var fs = require("fs");
+var inquirer = require("inquirer")
 var spotify = new Spotify(keys.spotify);
 var client = new Twitter(keys.twitter);
 
-console.log(spotify);
-console.log(client);
+
+//functions
+function tweets(){
+    var params = {screen_name:'Daniel60192083', count:20};
+    client.get("statuses/user_timeline",params,function(error,tweets,response){
+        if(!error &&response.statusCode===200){
+            // console.log(response.statuses);
+            for (var i = 0; i < 20; i++){
+                console.log("\n============================");
+                console.log("Tweet: " + tweets[i].text);
+                console.log("Tweeted at: " + tweets[i].created_at);
+                console.log("============================\n");
+            }
+        }
+    })
+};
+
+function songs(){
+    inquirer.prompt([
+        {
+        type: "input",
+        message:"What song would you like me to get?",
+        name:"song"
+        }
+
+    ]).then(function(getSongs){
+        var searched = getSongs.song;
+        console.log("searching...  " + searched)
+        if (searched === ""){
+            
+            console.log("I'm sorry, it appears you didn't search a song. Here's my favorite.");
+            console.log("Artist: The Sign" );
+            console.log("Album: Ace of Base");
+            console.log("Link: https://open.spotify.com/track/3oCJJksC12uFxkt3RQ7rbV");   
+            }
+        else {
+            spotify.search({type:"track", query:"'" + searched + "'"}, function(error, data){
+                console.log("Artist: " + data.tracks.items[0].artists[0].name);
+                console.log("Album: " + data.tracks.items[0].album.name);
+                console.log("Link: " + data.tracks.items[0].external_urls.spotify);
+            }); 
+        }
+    })
+
+}
+
+
+//inquirers 
+inquirer.prompt([
+    {
+        type:"list",
+        message:"Welcome to Liri. Please make a selection.",
+        choices: ["My Tweets", "Spotify This Song", "Movie This", "Do What It Says"],
+        name: "choice"
+    }
+]).then (function (inquirerResponse){
+    if (inquirerResponse.choice == "My Tweets"){
+        console.log("Twitter")
+        tweets();
+    }
+    else if (inquirerResponse.choice == "Spotify This Song"){
+            console.log("Spotify")
+            songs();
+    }
+    else if (inquirerResponse.choice == "Movie This"){
+            console.log("Movies")
+    }
+    else if (inquirerResponse.choice == "Do What It Says"){
+            console.log("Random")
+    }
+
+});
+
+
+// remember to npm install request and try using request, refer to 20 and 21
+// look to see if there needs to be npm install for APIs 
+// look at inquirer, activity 25
